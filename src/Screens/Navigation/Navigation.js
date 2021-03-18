5db8a6d4f1b66548c356e59bbff3bddd5c35090f
masdefry <muhammaddefryan@gmail.com>
2021-03-18 14:04:05 +0700

    Stack Navigation

--- src/Screens/Navigation/Navigation.js
+++ src/Screens/Navigation/Navigation.js
@@ -8,10 +8,12 @@ const StackNavigation = () => {
         <Stack.Navigator>
             <Stack.Screen name='Login' component={Login} />
             <Stack.Screen name='Register' component={Register} />
+            <Stack.Screen name='Home' component={HomeStackNavigation} options={{headerShown: false}} />
         </Stack.Navigator>
     )
 }
 
+// Screen Login
 const Login = ({navigation: {navigate}}) => {
     return(
         <View style={{height: '100%', justifyContent: 'center', alignItems: 'center'}}>
@@ -21,10 +23,15 @@ const Login = ({navigation: {navigate}}) => {
             <Text style={{color: 'blue', fontStyle: 'italic'}} onPress={() => navigate('Register')}>
                 Don't Have Account? Register!
             </Text>
+            <Text style={{color: 'blue', fontStyle: 'italic'}} onPress={() => navigate('Home')}>
+                Go to Home
+            </Text>
         </View>
     )
 }
 
+
+// Screen Register
 const Register = () => {
     return(
         <View style={{height: '100%', justifyContent: 'center', alignItems: 'center'}}>
@@ -35,7 +42,78 @@ const Register = () => {
     )
 }
 
+// ####################################################################################################
+
+const HomeStack = createStackNavigator()
+const HomeStackNavigation = () => {
+    return(
+        <HomeStack.Navigator>
+            <HomeStack.Screen name='Home' component={Home} />
+            <HomeStack.Screen name='Product' component={Product} />
+            <HomeStack.Screen name='Cart' component={Cart} />
+            <HomeStack.Screen name='Profile' component={Profile} />
+        </HomeStack.Navigator>
+    )
+}
+
+// Screen Home
+const Home = ({navigation: {navigate}}) => {
+    return(
+        <View style={{height: '100%', justifyContent: 'center', alignItems: 'center'}}>
+            <Text>
+                This is Home Screen
+            </Text>
+            <Text style={{color: 'blue', fontStyle: 'italic'}} onPress={() => navigate('Product')}>
+                PRODUCT
+            </Text>
+            <Text style={{color: 'blue', fontStyle: 'italic'}}>
+                CART
+            </Text>
+            <Text style={{color: 'blue', fontStyle: 'italic'}}>
+                PROFILE
+            </Text>
+        </View>
+    )
+}
+
+// Screen Product
+const Product = () => {
+    return(
+        <View style={{height: '100%', justifyContent: 'center', alignItems: 'center'}}>
+            <Text>
+                This is Product Screen
+            </Text>
+        </View>
+    )
+}
+
+// Screen Cart
+const Cart = () => {
+    return(
+        <View style={{height: '100%', justifyContent: 'center', alignItems: 'center'}}>
+            <Text>
+                This is Cart Screen
+            </Text>
+        </View>
+    )
+}
+
+// Screen Profile
+const Profile = () => {
+    return(
+        <View style={{height: '100%', justifyContent: 'center', alignItems: 'center'}}>
+            <Text>
+                This is Profile Screen
+            </Text>
+        </View>
+    )
+}
+
 export default StackNavigation
 
 // Login
 // Register
+// Home
+    // Product
+    // Cart
+    // Profile
