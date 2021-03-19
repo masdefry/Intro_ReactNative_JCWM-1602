2da86d9174247c1f07834f65daed3af9bc2d7926
masdefry <muhammaddefryan@gmail.com>
2021-03-19 11:16:03 +0700

    Styling Bottom Tab & Send Params With Routes

--- src/Screens/Navigation/Navigation.js
+++ src/Screens/Navigation/Navigation.js
@@ -2,6 +2,8 @@ import React from 'react'
 import { View, Text } from 'react-native'
 import { createStackNavigator } from '@react-navigation/stack'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import Icon from 'react-native-vector-icons/FontAwesome';
+import { useEffect } from 'react/cjs/react.development';
 
 const Stack = createStackNavigator()
 const StackNavigation = () => {
@@ -16,16 +18,22 @@ const StackNavigation = () => {
 
 // Screen Login
 const Login = ({navigation: {navigate}}) => {
+
+    let name = 'Defryan'
+
     return(
         <View style={{height: '100%', justifyContent: 'center', alignItems: 'center'}}>
             <Text>
                 This is Login Screen
             </Text>
-            <Text style={{color: 'blue', fontStyle: 'italic'}} onPress={() => navigate('Register')}>
+            <Text style={{color: 'blue', fontStyle: 'italic'}} onPress={() => navigate('Register', {sendName: name})}>
                 Don't Have Account? Register!
             </Text>
-            <Text style={{color: 'blue', fontStyle: 'italic'}} onPress={() => navigate('Home')}>
+            <Text style={{color: 'blue', fontStyle: 'italic'}} onPress={() => navigate('Home', {sendName: name})}>
                 Go to Home
+            </Text>
+            <Text>
+                Icon <Icon name='facebook' color='blue' />
             </Text>
         </View>
     )
@@ -33,11 +41,19 @@ const Login = ({navigation: {navigate}}) => {
 
 
 // Screen Register
-const Register = () => {
+const Register = ({route}) => {
+
+    useEffect(() => {
+        console.log(route.params.sendName)
+    }, [])
+
     return(
         <View style={{height: '100%', justifyContent: 'center', alignItems: 'center'}}>
             <Text>
                 This is Register Screen
+            </Text>
+            <Text style={{color: 'red', fontStyle: 'italic'}}>
+                This is Params From Login Screen : {route.params.sendName}
             </Text>
         </View>
     )
@@ -46,19 +62,54 @@ const Register = () => {
 // ####################################################################################################
 
 const HomeStack = createBottomTabNavigator()
-const HomeStackNavigation = () => {
+const HomeStackNavigation = ({route}) => {
+
+    useEffect(() => {
+        console.log(`${route.params.sendName} Baru Sampai Entry Point Home`)
+    }, [])
+
     return(
-        <HomeStack.Navigator>
-            <HomeStack.Screen name='Home' component={Home} />
-            <HomeStack.Screen name='Product' component={Product} />
-            <HomeStack.Screen name='Cart' component={Cart} />
-            <HomeStack.Screen name='Profile' component={ProfileStackNavigation} options={{headerShown: false}} />
+        <HomeStack.Navigator
+            tabBarOptions={{
+                activeTintColor: '#e45826',
+                activeBackgroundColor: '#0a043c',
+                size: 6
+            }}
+        >
+            <HomeStack.Screen name='Home' component={Home} initialParams={{sendNameFromEntry: route.params.sendName}}
+                options={{tabBarIcon: ({color, size}) => {
+                    return(
+                        <Icon name='home' color={color} size={size} />
+                    )
+                }}}
+            />
+            <HomeStack.Screen name='Product' component={Product} initialParams={{sendNameFromEntry: route.params.sendName}}
+                options={{tabBarIcon: ({color, size}) => {
+                    return(
+                        <Icon name='tags' color={color} size={size} />
+                    )
+                }}}
+            />
+            <HomeStack.Screen name='Cart' component={Cart}
+                options={{tabBarIcon: ({color, size}) => {
+                    return(
+                        <Icon name='shopping-bag' color={color} size={size} />
+                    )
+                }}}
+            />
+            <HomeStack.Screen name='Profile' component={ProfileStackNavigation} 
+                options={{headerShown: false, tabBarIcon: ({color, size}) => {
+                    return(
+                        <Icon name='user' color={color} size={size} />
+                    )
+                }}} 
+            />
         </HomeStack.Navigator>
     )
 }
 
 // Screen Home
-const Home = ({navigation: {navigate}}) => {
+const Home = ({route, navigation: {navigate}}) => {
     return(
         <View style={{height: '100%', justifyContent: 'center', alignItems: 'center'}}>
             <Text>
@@ -73,16 +124,27 @@ const Home = ({navigation: {navigate}}) => {
             <Text style={{color: 'blue', fontStyle: 'italic'}} onPress={() => navigate('Profile')}>
                 PROFILE
             </Text>
+            <Text style={{color: 'red', fontStyle: 'italic'}}>
+                Ini Params Yang Dikirim Dari Login : {route.params.sendNameFromEntry}
+            </Text>
         </View>
     )
 }
 
 // Screen Product
-const Product = () => {
+const Product = ({route}) => {
+
+    useEffect(() => {
+        console.log(`${route.params.sendNameFromEntry} Sudah Sampai Di Component Product`)
+    }, [])
+
     return(
         <View style={{height: '100%', justifyContent: 'center', alignItems: 'center'}}>
             <Text>
                 This is Product Screen
+            </Text>
+            <Text style={{color: 'red', fontStyle: 'italic'}}>
+                Ini Params Yang Dikirim Dari Login : {route.params.sendNameFromEntry}
             </Text>
         </View>
     )
@@ -165,7 +227,7 @@ const TransactionHistory = () => {
 
 export default StackNavigation
 
-// Login
+// Login ---> name='Defryan'
 // Register
 // Home
     // Product
